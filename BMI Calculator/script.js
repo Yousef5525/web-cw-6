7d494998bac549a512fe80f9b736905f7b665da8
يوسف علاء عبده <joealaa552005@gmail.com>
2022-03-20 11:46:18 +0300

    day 6 ex2-3

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,16 @@
-// هنا سوف تنشئ دالة calculate 
+// هنا سوف تنشئ دالة calculate
+function calculate() {
+  let weight = document.getElementById(`weight`).value;
+  let height = document.getElementById(`height`).value;
+
+  let BMI = weight / (height * height);
+  if (BMI < 18.5) {
+    status = `انت تعاني من نقص في الوزن`;
+  } else if (BMI > 18.5 && BMI < 25) {
+    status = `وزن صحي`;
+  }
+
+  BMI = BMI.toFixed[2];
+  document.getElementById(`result`).innerHTML = BMI;
+  document.getElementsByClassName(`comment`)[0].innerHTML = status;
+}
